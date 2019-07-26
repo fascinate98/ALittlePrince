43c242ed110b4d23ca0d1fb31ff4cbc3a3e8d8ff
dall4e <dall4e@users.noreply.github.com>
2019-07-26 14:17:59 +0900

    디버그로그지웟서

--- Assets/Script/rotateImage.cs
+++ Assets/Script/rotateImage.cs
@@ -41,16 +41,16 @@ public class rotateImage : MonoBehaviour
         Vector3 Center =  Camera.main.WorldToScreenPoint(transform.position);
         Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10);
 
-        //Debug.Log(Center.x + "," + Center.y);
+       
 
         float Angle1 = GetAngle(Center, mousePosition);  // 센터랑 마우스 현재위치 각도
         float Angle2 = GetAngle(Center, clickPos);   //센터랑 마우스 처음위치 각도
 
-        float Angle3 = Angle2 - Angle1;  //뺌
+        float Angle3 = Angle1 - Angle2;  //뺌
 
         transform.rotation = Quaternion.Euler(0, 0, Angle3);  //회전
 
-        Debug.Log(Angle1 + " , " + Angle2);
+       
     }
 
     public static float GetAngle(Vector3 vStart, Vector3 vEnd) //-180~180
